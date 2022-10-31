45fbe892fe86031a1c96bfa0f3463b562803d0bd
riven <rivenzhou@rektec.com.cn>
2022-10-31 20:40:37 +0800

    点评项目 搭建家里的环境

--- server/src/main/java/com/Guo/GuoYelp/config/MvcConfig.java
+++ server/src/main/java/com/Guo/GuoYelp/config/MvcConfig.java
@@ -9,7 +9,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
  *
  */
 @Configuration
-public class MVCConfig implements WebMvcConfigurer {
+public class MvcConfig implements WebMvcConfigurer {
     /**
      * 添加自定义的拦截器
      * @param registry
